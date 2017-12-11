42a768ac150af367924b24fa804682ea4fa788a6
Dmitri Djakov <dmitri.djakov@gmail.com>
2017-12-11 20:26:04 +0200

    11.12.2017

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-document.write()
+
+function myFunction() {
+    var x = document.getElementById("myForm").elements[0].value;
+    console.log(x)
+}
--- start.js
+++ start.js
@@ -1,21 +1,25 @@
 var JSZip = require('jszip');
 var Docxtemplater = require('docxtemplater');
-
 var fs = require('fs');
 var path = require('path');
-
 //Load the docx file as a binary
-var content = fs
-    .readFileSync(path.resolve(__dirname, 'input.docx'), 'binary');
-
+var content = fs.readFileSync(path.resolve('input.docx'), 'binary');
 var zip = new JSZip(content);
-
 var doc = new Docxtemplater();
 doc.loadZip(zip);
-
 //set the templateVariables
+
+function generateFile() {
+    var nimi = document.getElementById("mainForm").elements[0].value;
+    var kood = document.getElementById("mainForm").elements[1].value;
+    var elukoht = document.getElementById("mainForm").elements[2].value;
+     var mark = document.getElementById("mainForm").elements[3].value;
+     var kuupaev = document.getElementById("mainForm").elements[4].value;
+     var aeg = document.getElementById("mainForm").elements[5].value;
+     var  koht = document.getElementById("mainForm").elements[6].value;
+     var kirjeldus = document.getElementById("mainForm").elements[7].value;
 doc.setData({
-    first_name: 'John',
+    first_name: nimi,
     last_name: 'Doe',
     phone: '0652455478',
     description: 'New Website'
@@ -37,8 +41,9 @@ catch (error) {
     throw error;
 }
 
-var buf = doc.getZip()
-             .generate({type: 'nodebuffer'});
+var buf = doc.getZip().generate({type: 'nodebuffer'});
 
 // buf is a nodejs buffer, you can either write it to a file or do anything else with it.
-fs.writeFileSync(path.resolve(__dirname, 'output.docx'), buf);
+fs.writeFileSync(path.resolve('output.docx'), buf);
+};
+
