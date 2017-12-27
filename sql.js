c76f06f67fc4c8ff05fff88724edf5a4db1fa690
Dmitri Djakov <dmitri.djakov@gmail.com>
2017-12-27 09:05:12 +0200

    27.12.2017

--- sql.js
+++ sql.js
@@ -1,7 +1,26 @@
-var sqlite3 = require('sqlite3');
-let db = new sqlite3.Database('db/storage.db', sqlite3.OPEN_READWRITE, (err) => {
-  if (err) {
-    console.error(err.message);
-  }
-  console.log('Connected to the storage.db database.');
-});
+'use strict';
+
+const sqlite3 = require('sqlite3').verbose();
+ 
+let db = new sqlite3.Database('./db/sample.db');
+ 
+let sql = `select * from viitenumbrid where viitenum is not null limit 1`;
+
+
+function parser (query){
+
+    db.each(query, function(err, row) {
+        let referenceNumber = (row.viitenum);
+              return referenceNumber;
+    });
+	
+
+};
+
+let result = parser(sql);
+console.log(result);
+
+//Код выше, выдает undefined. Т.е. как я понял, значение переменной referenceNumber не вышло за пределы функции parser. Вопрос в том, как мне вернуть результаты запроса и присвоить их переменной result 
+
+
+
